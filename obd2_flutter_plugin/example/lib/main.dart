6ef85b7c9c2c10f2eff3b5da2a10aaf1cc9eca8e
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-11-26 23:40:22 +0200

    impr: improved example app

--- obd2_flutter_plugin/example/lib/main.dart
+++ obd2_flutter_plugin/example/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:obd2_flutter_plugin/obd2_flutter_plugin.dart';
+import 'package:obd2_flutter_plugin/obd2_flutter_plugin_constants.dart';
 import 'logger.dart';
 
 void main() {
@@ -18,48 +20,17 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _fuelLevel = "--";
-  List<Object?> devices = List.empty();
-  final _obd2FlutterPlugin = Obd2FlutterPlugin();
   final logger = Logger("MyApp");
+  final _obd2FlutterPlugin = Obd2FlutterPlugin();
+
+  Iterable<BluetoothDevice> bleDevices = [];
+  String? adapterAddress;
+  bool adapterInitialized = false;
+  String _fuelLevel = "--";
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    // //* Get bounded bluetooth device
-    // try {
-    //   devices = await _obd2FlutterPlugin.getBluetoothDevices() ?? List.empty();
-    //   logger.log("Got devices: $devices");
-    // } on PlatformException {
-    //   logger.log("Error getting bluetooth devices");
-    //   return;
-    // }
-
-    // // todo: Connect to target device
-
-    // // todo: Init the target device
-
-    // //* Fuel level
-    // String fuelLevel;
-    // try {
-    //   fuelLevel = await _obd2FlutterPlugin.getFuelLevel() ?? "-1";
-    // } on PlatformException {
-    //   fuelLevel = "UNKNOWN";
-    // }
-
-    // // If the widget was removed from the tree while the asynchronous platform
-    // // message was in flight, we want to discard the reply rather than calling
-    // // setState to update our non-existent appearance.
-    // if (!mounted) return;
-
-    // setState(() {
-    //   _fuelLevel = fuelLevel;
-    // });
   }
 
   @override
@@ -67,60 +38,91 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('iOS OBD Plugin sample app'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              ElevatedButton(
+              CupertinoButton(
                 onPressed: () async {
                   try {
-                    var devices = await _obd2FlutterPlugin.getBluetoothDevices() ?? List.empty();
-                    logger.log("Got devices: $devices");
+                    // Resolve received devices
+                    var rawDevices = await _obd2FlutterPlugin.getBluetoothDevices();
+                    var devices = rawDevices.map<BluetoothDevice>((d) => BluetoothDevice.fromJson(d as String? ?? ""));
+                    logger.log("Got: raw=${rawDevices.length} -> resolved=${devices.length} Devices");
+                    // Try to find the adapter by its name in the list
+                    String? tempAddress;
+                    for (var device in devices) {
+                      logger.log("Trying device: ${device.name} at ${device.address}");
+                      if (device.name == OBD_ADAPTER_NAME) {
+                        tempAddress = device.address;
+                        logger.log("Found adapter ${device.name} at address ${device.address}");
+                        break;
+                      }
+                    }
+                    setState(() {
+                      bleDevices = devices;
+                      if (tempAddress != null) adapterAddress = tempAddress;
+                    });
                   } on PlatformException catch (e) {
-                    logger.log("Error getting bluetooth devices. $e");
+                    logger.log("Error getting bluetooth devices.\nReason: $e");
                   }
                 },
-                child: Text('Scan bluetooth devices'),
+                child: Text(bleDevices.isNotEmpty ? "Found ${bleDevices.length} device" : "Scan Bluetooth for devices"),
               ),
-              ElevatedButton(
-                onPressed: ()async{
+              CupertinoButton(
+                onPressed: () async {
                   try {
-                    var connected = await _obd2FlutterPlugin.connect("123-456-789");
-                    logger.log("Connection to adapter: $connected");
-                  } on PlatformException {
-                    logger.log("Can't connect to adapter");
+                    var connected = await _obd2FlutterPlugin.connect(adapterAddress ?? "") ?? false;
+                    logger.log(connected ? "Connected to adapter" : "Can't connect to adapter");
+                  } on PlatformException catch (e) {
+                    logger.log("Error connecting to adapter.\nReason: $e");
                   }
                 },
-                child: Text('Connect to adapter'),
+                child: Text(adapterAddress != null ? 'Connect to ${adapterAddress ?? ""}' : ""),
               ),
-              ElevatedButton(
-                onPressed: () async{
+              CupertinoButton(
+                onPressed: () async {
                   try {
                     await _obd2FlutterPlugin.init();
-                  } on PlatformException {
-                    logger.log("Can't initialize adapter");
+                  } on PlatformException catch (e) {
+                    logger.log("Can't initialize adapter..\nReason: $e");
                   }
                 },
-                child: Text('Initialize adapter'),
+                child: const Text('Initialize adapter'),
               ),
-              ElevatedButton(
-                onPressed: () async{
+              CupertinoButton(
+                onPressed: () async {
                   try {
-                    var fuel = await _obd2FlutterPlugin.getFuelLevel();
-                    logger.log("Got fuel level: $fuel");
-                  } on PlatformException {
-                    logger.log("Can't get fuel level");
+                    var fuelLevel = await _obd2FlutterPlugin.getFuelLevel();
+                    logger.log("Got fuel level: $fuelLevel");
+                    setState(() {
+                      _fuelLevel = fuelLevel ?? "Unknown";
+                    });
+                  } on PlatformException catch (e) {
+                    logger.log("Can't get fuel level..\nReason: $e");
                   }
                 },
-                child: Text('Get fuel level'),
+                child: Text(_fuelLevel == "--" ? "Get fuel level" : "FuelLevel: $_fuelLevel"),
               ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class BluetoothDevice {
+  String name;
+  String address;
+
+  BluetoothDevice({required this.name, required this.address});
+
+  static BluetoothDevice fromJson(String jsonDevice) {
+    final Map<String, String> parsedDevice = json.decode(jsonDevice);
+    return BluetoothDevice(name: parsedDevice['name'] ?? "", address: parsedDevice['address'] ?? "");
   }
 }
