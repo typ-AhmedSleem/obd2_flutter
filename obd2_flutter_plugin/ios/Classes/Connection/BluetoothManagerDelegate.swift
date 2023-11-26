a7dd95a9e1459626cf77063653da976bff7aaec8
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-11-26 23:40:49 +0200

    impr: improved BluetoothManager with some new features

--- obd2_flutter_plugin/ios/Classes/Connection/BluetoothManagerDelegate.swift
+++ obd2_flutter_plugin/ios/Classes/Connection/BluetoothManagerDelegate.swift
@@ -4,7 +4,7 @@ protocol BluetoothManagerDelegate {
 
      func onAdapterConnected()
      func onAdapterInitialized()
-    func onAdapterStateChanged(state: Int)
+     func onAdapterStateChanged(state: Int)
      func onAdapterDisconnected()
      func onAdapterReceiveResponse(response: String?)
 
