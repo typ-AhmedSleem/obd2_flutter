63a7625aa444ccba18ef37ba92b9102d27b3f7c4
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2024-01-02 01:03:34 +0200

    macos: added macOS platform to project root

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import obd2_flutter_plugin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  macOS_OBD2FlutterPlugin.register(with: registry.registrar(forPlugin: "macOS_OBD2FlutterPlugin"))
+}
